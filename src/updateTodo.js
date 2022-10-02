91247aee316cc00f7b8ccf1c09e52e0d0847d371
cohoichota <cohoichota@gmail.com>
2022-10-02 17:13:03 +0700

    Initial commit

---
+++ src/updateTodo.js
@@ -0,0 +1,33 @@
+const AWS = require("aws-sdk");
+const middy = require("@middy/core");
+const httpJsonBodyParser = require("http-json-body-parser");
+
+const updateTodo = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { completed } = event.body;
+  const { id } = event.pathParameters;
+
+  await dynamodb
+    .update({
+      TableName: "TodoTable",
+      Key: { id },
+      UpdateExpression: "set completed = :completed",
+      ExpressionAttributeValues: {
+        ":completed": completed,
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: "Todo Updated",
+    }),
+  };
+};
+
+module.exports = {
+  handler: middy(updateTodo).use(httpJsonBodyParser()),
+};
